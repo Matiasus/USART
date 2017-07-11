c7436683afd9d835b6ccec43a82e0e644474bebf
Marián <Matiasus@users.noreply.github.com>
2017-07-11 15:25:27 +0200

    Create uart.c

---
+++ uart.c
@@ -0,0 +1,24 @@
+/** 
+ * UART Communication
+ *
+ * Copyright (C) 2016 Marian Hrinko.
+ * Written by Marian Hrinko (mato.hrinko@gmail.com)
+ *
+ * @author      Marian Hrinko
+ * @datum       11.07.2017
+ * @file        uart.c
+ * @tested      AVR Atmega16
+ * @inspiration
+ * ---------------------------------------------------
+ */
+ 
+ /**
+ * @description Initialise UART communication
+ *
+ * @param  void
+ * @return void
+ */
+ void UartInit (void)
+ {
+ 
+ }
