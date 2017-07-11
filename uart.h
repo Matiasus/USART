e471612489f045bf6bbd1f2c5c5baac8a7806fd9
Marián <Matiasus@users.noreply.github.com>
2017-07-11 15:27:47 +0200

    Create uart.h

---
+++ uart.h
@@ -0,0 +1,26 @@
+/** 
+ * UART Communication
+ *
+ * Copyright (C) 2016 Marian Hrinko.
+ * Written by Marian Hrinko (mato.hrinko@gmail.com)
+ *
+ * @author      Marian Hrinko
+ * @datum       11.07.2017
+ * @file        uart.h
+ * @tested      AVR Atmega16
+ * @inspiration
+ * ---------------------------------------------------
+ */
+ 
+#ifndef __UART_H__
+#define __UART_H__
+
+  /**
+   * @description Init UART
+   * 
+   * @param  void
+   * @return void
+   */
+  void DelayMs(uint8_t);
+
+#endif
