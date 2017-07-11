c7fd605a90147eed642faa42f812928d91d4d76c
Marián <Matiasus@users.noreply.github.com>
2017-07-11 15:43:15 +0200

    Update uart.c

--- uart.c
+++ uart.c
@@ -11,14 +11,28 @@
  * @inspiration
  * ---------------------------------------------------
  */
- 
- /**
+
+#include <avr/io.h>
+/**
  * @description Initialise UART communication
  *
  * @param  void
  * @return void
  */
- void UartInit (void)
- {
- 
- }
+void UartInit (void)
+{
+  // definition of baud rate - speed of transmition
+  // write high byte 
+  UBRRH = (unsigned char) (UBRR_VAL >> 8);
+  // write low byte
+  UBRRL = (unsigned char) UBRR_VAL;
+  // Enable receiving and transmitting
+  USCRB = (1 << RXEN) 
+        | (1 << TXEN);
+  // set frame format 8 bits, even parity, 2 stop bits, 
+  UCSRC = (1 << URSEL)
+        | (1 << UCSZ0)
+        | (1 << UCSZ1)
+        | (1 << UPM1)
+        | (1 << USBS);
+}
